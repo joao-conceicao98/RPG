4a14a99f55a7ecd0090b8c2f640de92ff17a91d9
BRYCK <32332929+BRYCK@users.noreply.github.com>
2017-10-16 00:09:46 +0100

    2
    moviment
    player animation
    maptles

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,13 +1,19 @@
 extends KinematicBody2D
 
 export var MOTION_SPEED = 140
+const IDLE_SPEED = 10
 
 var RayNode
+
+var PlayerAnimNode
+var anim = ""
+var animNew = ""
 
 func _ready():
 	set_fixed_process(true)
 	
 	RayNode = get_node("RayCast2D")
+	PlayerAnimNode = get_node("AnimatedSprite")
 
 
 func _fixed_process(delta):
@@ -33,3 +39,30 @@ func _fixed_process(delta):
 	
 	motion = motion.normalized()*MOTION_SPEED*delta
 	move(motion)
+	
+	#Animations
+	
+	
+	if (motion.length() > IDLE_SPEED*0.09):
+		if (Input.is_action_pressed("ui_up")):
+			anim = "Walk_U"
+		if (Input.is_action_pressed("ui_down")):
+			anim = "Walk_D"
+		if (Input.is_action_pressed("ui_right")):
+			anim = "Walk_R"
+		if (Input.is_action_pressed("ui_left")):
+			anim = "Walk_L"
+	else:
+		if (RayNode.get_rotd() == 180):
+			anim = "Idle_U"
+		if (RayNode.get_rotd() == 0):
+			anim = "Idle_D"
+		if (RayNode.get_rotd() == -90):
+			anim = "Idle_L"
+		if (RayNode.get_rotd() == 90):
+			anim = "Idle_R"
+	
+	
+	if anim != animNew:
+		animNew = anim
+		PlayerAnimNode.play(anim)
